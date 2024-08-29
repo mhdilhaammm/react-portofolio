a6bc74cd6b37ffe2fb593cabf4b69a7888e3ba78
DeathClawn <killerclawn11@gmail.com>
2024-08-29 23:29:03 +0700

    add animation framer motion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import HomeLayout from '../Components/Layouts/HomeLayout'
+import { motion, useScroll } from "framer-motion";
+import "../pages/style.css"
 
 const Home = () => {
+    const { scrollYProgress } = useScroll();
+
     return (
-        <HomeLayout />
+        <>
+            <HomeLayout />
+            <motion.div className='progress-bar' style={{ scaleX: scrollYProgress }} />
+        </>
     )
 }
 
